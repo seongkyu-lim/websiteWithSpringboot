90c5bd666ad03fdf14b809cc7d19a10465d4ffeb
limseongkyu <96x60812@naver.com>
2021-02-10 00:04:26 +0900

    [Ing] 수정, 삭

--- src/main/java/com/seongkyu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/seongkyu/book/springboot/web/PostsApiController.java
@@ -3,10 +3,7 @@ package com.seongkyu.book.springboot.web;
 import com.seongkyu.book.springboot.service.posts.PostsService;
 import com.seongkyu.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,5 +14,15 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestsDto requestsDto) {
+        return postsService.update(id, requestsDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
     }
 }
---
+++ src/main/java/com/seongkyu/book/springboot/web/dto/PostResponseDto.java
@@ -0,0 +1,19 @@
+package com.seongkyu.book.springboot.web.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PostResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
