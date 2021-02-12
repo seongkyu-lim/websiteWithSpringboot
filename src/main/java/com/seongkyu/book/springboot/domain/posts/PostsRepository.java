7988f2d5f03d87b8e6849f083e46ffb58112965f
limseongkyu <96x60812@naver.com>
2021-02-12 22:25:10 +0900

    [Finished] chapter 4

--- src/main/java/com/seongkyu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/seongkyu/book/springboot/domain/posts/PostsRepository.java
@@ -1,9 +1,15 @@
 package com.seongkyu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts,Long> {
 
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+
+    //springDataJpa에서 제공하지 않는 메소드는 위처럼 쿼리로 작성
 }
 
 // DB layer 접근자.
--- src/test/java/com/seongkyu/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/seongkyu/book/springboot/web/PostsApiControllerTest.java
@@ -18,6 +18,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
